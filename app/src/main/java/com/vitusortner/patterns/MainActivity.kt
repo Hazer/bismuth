6a1bc95cc70f876c0e606d6adf326133ef595ed6
vitusortner <vitusortner.dev@gmail.com>
2018-07-18 22:31:07 +0200

    First intent handling + some dependencies

--- app/src/main/java/com/vitusortner/patterns/MainActivity.kt
+++ app/src/main/java/com/vitusortner/patterns/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.vitusortner.patterns
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private val log = Logger(javaClass)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val action = intent.action
+        val data = intent.data
+
+        log.i("Action: $action, data: $data")
     }
 }
