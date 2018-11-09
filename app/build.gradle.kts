19b3d8875c628ae080e5dd0d83a43f5f7fd7b61f
Vitus <vitusortner.dev@gmail.com>
2018-11-09 17:36:26 +0100

    Use Gradle Kotlin DSL (#2)

---
+++ app/build.gradle.kts
@@ -0,0 +1,71 @@
+import org.jetbrains.kotlin.config.KotlinCompilerVersion
+import org.jetbrains.kotlin.gradle.internal.AndroidExtensionsExtension
+
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("kotlin-android-extensions")
+}
+
+androidExtensions {
+    configure(delegateClosureOf<AndroidExtensionsExtension> {
+        isExperimental = true
+    })
+}
+
+android {
+    compileSdkVersion(28)
+
+    defaultConfig {
+        applicationId = "com.vitusortner.patterns"
+        minSdkVersion(21)
+        targetSdkVersion(28)
+        versionCode = 1
+        versionName = "1.0"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+        }
+    }
+}
+
+dependencies {
+    val lifecycleVersion = "2.0.0"
+    val coroutinesVersion = "1.0.0-RC1"
+    val moshiVersion = "2.4.0"
+
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+    implementation(kotlin("stdlib-jdk7", KotlinCompilerVersion.VERSION))
+
+
+    implementation("androidx.appcompat:appcompat:1.0.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.0.0-alpha2")
+    implementation("androidx.recyclerview:recyclerview:1.0.0")
+    implementation("androidx.lifecycle:lifecycle-extensions:$lifecycleVersion")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutinesVersion")
+
+    implementation("com.squareup.retrofit2:retrofit:$moshiVersion")
+    implementation("com.squareup.retrofit2:converter-moshi:$moshiVersion")
+    implementation("com.squareup.retrofit2:converter-scalars:$moshiVersion")
+
+    implementation("com.jakewharton.retrofit:retrofit2-kotlin-coroutines-adapter:0.9.2")
+
+    implementation("com.squareup.moshi:moshi-kotlin:1.7.0")
+    implementation("com.serjltt.moshi:moshi-lazy-adapters:2.2")
+
+    implementation("com.squareup.picasso:picasso:2.71828")
+
+    testImplementation("junit:junit:4.12")
+    testImplementation("androidx.arch.core:core-testing:$lifecycleVersion")
+    testImplementation("io.mockk:mockk:1.8.12.kotlin13")
+    testImplementation("org.amshove.kluent:kluent-android:1.42")
+
+    androidTestImplementation("androidx.test:runner:1.1.0")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.1.0")
+}
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,14 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 buildscript {
-    ext.kotlin_version = '1.3.0'
     repositories {
         google()
         jcenter()
     }
+
     dependencies {
-        classpath 'com.android.tools.build:gradle:3.4.0-alpha02'
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
+        classpath("com.android.tools.build:gradle:3.4.0-alpha02")
+        classpath(kotlin("gradle-plugin", version = "1.3.0"))
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
@@ -22,6 +22,6 @@ allprojects {
     }
 }
 
-task clean(type: Delete) {
-    delete rootProject.buildDir
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
 }
